156d1f10d3325d58dd1c682fdde6a8d68f07e3b6
Emeghiebo Chizoba <chizobaemeh09@gmail.com>
2024-07-07 19:29:28 +0100

    completed game logic

--- js/main.js
+++ js/main.js
@@ -1,18 +1,62 @@
 let rock = document.querySelector('.rock')
 const paper = document.querySelector('.paper')
 const scissors = document.querySelector('.scissors')
+const choice = document.querySelectorAll('.choice')
+const win = document.querySelector('.win')
+const play = document.querySelector('.play')
+const option = document.querySelector('.option')
+let computerScore = 0
+let humanScore = 0
 
 // Create a function named getComputerchoice
 
 function getComputerChoice() {
-  let computerChoice = Math.floor(Math.random() * 10) + 1
-  if (computerChoice <= 3) {
-    console.log('rock')
-  } else if (computerChoice > 3 && computerChoice <= 6) {
-    console.log('paper')
+  let computerChoice = Math.floor(Math.random() * 3) + 1
+  if (computerChoice == 1) {
+    return 'rock'
+  } else if (computerChoice == 2) {
+    return 'paper'
   } else {
-    console.log('scissors')
+    return 'scissors'
   }
 }
 
-getComputerChoice()
+// getComputerChoice()
+
+// Get player choice
+play.addEventListener('click', () => {
+  let computer = getComputerChoice()
+
+  if (option.value === computer) {
+    console.log(`It's a tie!!!!!!!!!!!!!!!`)
+  } else if (option.value == 'rock' && computer == 'scissors') {
+    console.log(`you win`)
+  } else if (option.value == 'paper' && computer == 'rock') {
+    console.log(`you win`)
+  } else if (option.value == 'scissors' && computer == 'paper') {
+    console.log(`you win`)
+  } else {
+    console.log('computer wins')
+  }
+})
+
+// get playerChoice
+
+choice.forEach((ch) => {
+  ch.addEventListener('click', () => {
+    playerChoice = ch.innerHTML
+    let computer = getComputerChoice()
+
+    if (playerChoice === computer) {
+      console.log(`It's a tie!!!!!!!!!!!!!!!`)
+    } else if (playerChoice == 'rock' && computer == 'scissors') {
+      console.log(`you win`)
+    } else if (playerChoice == 'paper' && computer == 'rock') {
+      console.log(`you win`)
+    } else if (playerChoice == 'scissors' && computer == 'paper') {
+      console.log(`you win`)
+    } else {
+      console.log('computer wins')
+    }
+  })
+})
