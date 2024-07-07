49e5e27c116491d28c6c26409de8a6d0e0b24b2e
Emeghiebo Chizoba <chizobaemeh09@gmail.com>
2024-07-07 19:46:02 +0100

    game score works as expected

--- js/main.js
+++ js/main.js
@@ -2,9 +2,9 @@ let rock = document.querySelector('.rock')
 const paper = document.querySelector('.paper')
 const scissors = document.querySelector('.scissors')
 const choice = document.querySelectorAll('.choice')
+let comscore = document.querySelector('.comscore')
+let humscore = document.querySelector('.humscore')
 const win = document.querySelector('.win')
-const play = document.querySelector('.play')
-const option = document.querySelector('.option')
 let computerScore = 0
 let humanScore = 0
 
@@ -21,25 +21,6 @@ function getComputerChoice() {
   }
 }
 
-// getComputerChoice()
-
-// Get player choice
-play.addEventListener('click', () => {
-  let computer = getComputerChoice()
-
-  if (option.value === computer) {
-    console.log(`It's a tie!!!!!!!!!!!!!!!`)
-  } else if (option.value == 'rock' && computer == 'scissors') {
-    console.log(`you win`)
-  } else if (option.value == 'paper' && computer == 'rock') {
-    console.log(`you win`)
-  } else if (option.value == 'scissors' && computer == 'paper') {
-    console.log(`you win`)
-  } else {
-    console.log('computer wins')
-  }
-})
-
 // get playerChoice
 
 choice.forEach((ch) => {
@@ -48,15 +29,23 @@ choice.forEach((ch) => {
     let computer = getComputerChoice()
 
     if (playerChoice === computer) {
-      console.log(`It's a tie!!!!!!!!!!!!!!!`)
+      win.innerHTML = `It's a tie!!!!!!!!!!!!!!!`
     } else if (playerChoice == 'rock' && computer == 'scissors') {
-      console.log(`you win`)
+      humanScore++
+      humscore.innerHTML = Number(humanScore)
+      win.innerHTML = `you win`
     } else if (playerChoice == 'paper' && computer == 'rock') {
-      console.log(`you win`)
+      humanScore++
+      humscore.innerHTML = Number(humanScore)
+      win.innerHTML = `you win`
     } else if (playerChoice == 'scissors' && computer == 'paper') {
-      console.log(`you win`)
+      humanScore++
+      humscore.innerHTML = Number(humanScore)
+      win.innerHTML = `you win`
     } else {
-      console.log('computer wins')
+      computerScore++
+      comscore.innerHTML = Number(computerScore)
+      win.innerHTML = 'computer wins'
     }
   })
 })
