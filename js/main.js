5e91ebbc0f9f80ae22d40367d089aacd9a980c64
Emeghiebo Chizoba <chizobaemeh09@gmail.com>
2024-07-12 10:41:55 +0100

    Game over logic done

--- js/main.js
+++ js/main.js
@@ -9,41 +9,13 @@ const win = document.querySelector('.win')
 let computerScore = 0
 let humanScore = 0
 
+// game emotions???
+const loser = ['You loser', 'Brudda ugggg', 'Give up', 'You can never make it', 'You should be ashamed of yourself']
+const winner = ['Champ', "Let's goooo", 'Winnnerrrrrr', 'You should be proud of yourself', 'It GIRL!!!!!!!!!!!']
+
 // get player name
 let player = prompt('Enter your name: ')
 playerName.innerHTML = `${player}!`
-
-// get playerChoice
-choice.forEach((ch) => {
-  ch.addEventListener('click', () => {
-    playerChoice = ch.id
-    let computer = getComputerChoice()
-
-    if (playerChoice === computer) {
-      win.innerHTML = `It's a tie!!!!!!!!!!!!!!!`
-    } else if (playerChoice == 'rock' && computer == 'scissors') {
-      humanScore++
-      humscore.innerHTML = Number(humanScore)
-      win.innerHTML = `you win`
-    } else if (playerChoice == 'paper' && computer == 'rock') {
-      humanScore++
-      humscore.innerHTML = Number(humanScore)
-      win.innerHTML = `you win`
-    } else if (playerChoice == 'scissors' && computer == 'paper') {
-      humanScore++
-      humscore.innerHTML = Number(humanScore)
-      win.innerHTML = `you win`
-    } else {
-      computerScore++
-      comscore.innerHTML = Number(computerScore)
-      win.innerHTML = 'computer wins'
-    }
-
-    // show stats
-    let stat = document.querySelector('.stat')
-    stat.innerHTML = `${player}: ${playerChoice} <br> Computer: ${computer}`
-  })
-})
 
 // Create a function named getComputerchoice
 function getComputerChoice() {
@@ -57,7 +29,47 @@ function getComputerChoice() {
   }
 }
 
+// get playerChoice
+choice.forEach((ch) => {
+  ch.addEventListener('click', () => {
+    playerChoice = ch.id
+    let computer = getComputerChoice()
+
+    if (playerChoice === computer) {
+      win.innerHTML = `It's a tie!!!!!!!!!!!!!!!`
+    } else if (playerChoice == 'rock' && computer == 'scissors') {
+      humanScore++
+      humscore.innerHTML = Number(humanScore)
+      win.innerHTML = `You win!`
+    } else if (playerChoice == 'paper' && computer == 'rock') {
+      humanScore++
+      humscore.innerHTML = Number(humanScore)
+      win.innerHTML = `You win!`
+    } else if (playerChoice == 'scissors' && computer == 'paper') {
+      humanScore++
+      humscore.innerHTML = Number(humanScore)
+      win.innerHTML = `You win!`
+    } else {
+      computerScore++
+      comscore.innerHTML = Number(computerScore)
+      win.innerHTML = 'Computer wins'
+    }
+
+    // show stats
+    let stat = document.querySelector('.stat')
+    stat.innerHTML = `${player}: ${playerChoice} <br> Computer: ${computer}`
+
+    // game over logic
+    let rand = Math.floor(Math.random() * 5)
+    if (computerScore >= 5) {
+      alert(`Computer wins. ${loser[rand]}`)
+      // TODO:REFRESH PAGE
+      location.reload()
+    } else if (humanScore >= 5) {
+      alert(`${player} wins.  ${winner[rand]}`)
+      location.reload()
+    }
+  })
+})
+
 // Work on gaame over
-// if (computerScore || humanScore == 5) {
-//   win.innerHTML = 'Game Over!!!!!!!!!!!!!!'
-// }
